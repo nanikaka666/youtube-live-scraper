1a871db560ecd174577ceb5c9309226fbb7cee6a
nanikaka <nanikaka666@gmail.com>
2025-10-01 13:53:59 +0900

    add test for pages and streams page fetcher

--- test/service/PageFetcher.test.ts
+++ test/service/PageFetcher.test.ts
@@ -5,7 +5,9 @@ import {
   ChannelPage,
   getChannelPage,
   getLivePage,
+  getStreamsPage,
   getVideoPage,
+  getVideosPage,
   VideoPage,
 } from "../../src/service/PageFetcher";
 
@@ -163,6 +165,88 @@ describe("getChannelPage", () => {
 
     expect(jest.mocked(fetch.fetchAsString).mock.calls[0][0]).toEqual(
       "https://www.youtube.com/channel/UCQ78z42ZYZHLlCiDexample",
+    );
+  });
+});
+
+describe("getVideosPage", () => {
+  test.each([new ChannelId("@example"), new ChannelId("UCQ78z42ZYZHLlCiDexample")])(
+    "successfully fetch the videos page.",
+    async (channelId) => {
+      jest
+        .spyOn(fetch, "fetchAsString")
+        .mockImplementation(jest.fn(() => Promise.resolve("<html></html>")));
+
+      const actual = await getVideosPage(channelId);
+
+      expect(actual.type).toEqual("videos");
+      expect(actual.channelId).toEqual(channelId);
+      expect(actual.html).toEqual("<html></html>");
+    },
+  );
+
+  test("check url passed to infrastructure layer, in case of Youtube handle.", async () => {
+    jest
+      .spyOn(fetch, "fetchAsString")
+      .mockImplementation(jest.fn(() => Promise.resolve("<html></html>")));
+
+    const actual = await getVideosPage(new ChannelId("@example"));
+
+    expect(jest.mocked(fetch.fetchAsString).mock.calls[0][0]).toEqual(
+      "https://www.youtube.com/@example/videos",
+    );
+  });
+
+  test("check url passed to infrastructure layer, in case of non Youtube handle.", async () => {
+    jest
+      .spyOn(fetch, "fetchAsString")
+      .mockImplementation(jest.fn(() => Promise.resolve("<html></html>")));
+
+    const actual = await getVideosPage(new ChannelId("UCQ78z42ZYZHLlCiDexample"));
+
+    expect(jest.mocked(fetch.fetchAsString).mock.calls[0][0]).toEqual(
+      "https://www.youtube.com/channel/UCQ78z42ZYZHLlCiDexample/videos",
+    );
+  });
+});
+
+describe("getStreamsPage", () => {
+  test.each([new ChannelId("@example"), new ChannelId("UCQ78z42ZYZHLlCiDexample")])(
+    "successfully fetch the streams page.",
+    async (channelId) => {
+      jest
+        .spyOn(fetch, "fetchAsString")
+        .mockImplementation(jest.fn(() => Promise.resolve("<html></html>")));
+
+      const actual = await getStreamsPage(channelId);
+
+      expect(actual.type).toEqual("streams");
+      expect(actual.channelId).toEqual(channelId);
+      expect(actual.html).toEqual("<html></html>");
+    },
+  );
+
+  test("check url passed to infrastructure layer, in case of Youtube handle.", async () => {
+    jest
+      .spyOn(fetch, "fetchAsString")
+      .mockImplementation(jest.fn(() => Promise.resolve("<html></html>")));
+
+    const actual = await getStreamsPage(new ChannelId("@example"));
+
+    expect(jest.mocked(fetch.fetchAsString).mock.calls[0][0]).toEqual(
+      "https://www.youtube.com/@example/streams",
+    );
+  });
+
+  test("check url passed to infrastructure layer, in case of non Youtube handle.", async () => {
+    jest
+      .spyOn(fetch, "fetchAsString")
+      .mockImplementation(jest.fn(() => Promise.resolve("<html></html>")));
+
+    const actual = await getStreamsPage(new ChannelId("UCQ78z42ZYZHLlCiDexample"));
+
+    expect(jest.mocked(fetch.fetchAsString).mock.calls[0][0]).toEqual(
+      "https://www.youtube.com/channel/UCQ78z42ZYZHLlCiDexample/streams",
     );
   });
 });
