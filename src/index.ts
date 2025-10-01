5fc1f372b1facc1c3dccef89ee76561e71459f17
nanikaka <nanikaka666@gmail.com>
2025-10-02 04:04:39 +0900

    put module entry point file

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { PageFetcher } from "./service/PageFetcher";
+import { Scraper } from "./service/Scraper";
+import { VideoListScraper } from "./service/VideoListScraper";
+import { ChannelId } from "./core/ChannelId";
+import { ChannelTitle } from "./core/ChannelTitle";
+import { VideoId } from "./core/VideoId";
+import { VideoTitle } from "./core/VideoTitle";
+
+export { PageFetcher, Scraper, VideoListScraper, ChannelId, ChannelTitle, VideoId, VideoTitle };
